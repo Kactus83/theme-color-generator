8042639cc67265bb1fb9bb94dd633575d1a89d0d
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-10-19 12:52:37 +0200

    Upgrade UI

--- color-generator/services/ThemeDisplayService.ts
+++ color-generator/services/ThemeDisplayService.ts
@@ -3,30 +3,25 @@ import { ThemeColors } from "../models/themes/ThemeColors";
 import { Color } from "../models/types/Colors";
 
 export class ThemeDisplayService {
-    constructor(private outputElement: HTMLElement) {}
-  
-    display(theme: ThemeColors) { // Remplacer `any` par le type approprié de votre thème
-      if (!this.outputElement) return;
-  
-      // Efface l'ancien affichage
-      this.outputElement.innerHTML = '';
-  
-      theme.palettes.forEach((palette: ColorsPalette) => { // Remplacer `any` par le type approprié de votre palette
-        const paletteDiv = document.createElement('div');
-  
-        paletteDiv.classList.add('palette');
-        paletteDiv.style.display = 'flex';
-  
-        palette.shades.forEach((shade: Color) => {
-          const colorDiv = document.createElement('div');
-          colorDiv.style.backgroundColor = shade.toString();
-          colorDiv.style.width = '50px';
-          colorDiv.style.height = '50px';
-          paletteDiv.appendChild(colorDiv);
-        });
-  
-        this.outputElement.appendChild(paletteDiv);
+  constructor(private outputElement: HTMLElement) {}
+
+  display(theme: ThemeColors) {
+    if (!this.outputElement) return;
+
+    this.outputElement.innerHTML = '';
+
+    theme.palettes.forEach((palette: ColorsPalette) => {
+      const paletteDiv = document.createElement('div');
+      paletteDiv.classList.add('palette');
+
+      palette.shades.forEach((shade: Color) => {
+        const colorDiv = document.createElement('div');
+        colorDiv.style.backgroundColor = shade.toString();
+        colorDiv.classList.add('color-box'); 
+        paletteDiv.appendChild(colorDiv);
       });
-    }
+
+      this.outputElement.appendChild(paletteDiv);
+    });
   }
-  
+}
