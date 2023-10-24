a6c4e6fc5b7a84146b8ca8daf9ae7862d6e897be
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-10-24 07:12:33 +0200

    WIP adding new number of colors modes

--- color-generator/models/types/ThemeNumberOfColors.ts
+++ color-generator/models/types/ThemeNumberOfColors.ts
@@ -1,5 +1,9 @@
 export enum ThemeNumberOfColors {
     SIMPLE = 'simple',
-    BI_COLOR = 'bi-colors',
-    TRI_COLOR = 'tri-colors',
+    BI_COLOR_UP = 'bi-colors-up',
+    TRI_COLOR_UP = 'tri-colors-up',
+    BI_COLOR_DOWN = 'bi-colors-down',
+    TRI_COLOR_DOWN = 'tri-colors-down',
+    BI_COLOR_CENTERED = 'bi-colors-centered',
+    TRI_COLOR_CENTERED = 'tri-colors-centered',
 }
